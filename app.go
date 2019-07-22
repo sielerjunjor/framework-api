5e32fee2e2caa3eff041c99d16e6f1be9aacc423
sielerjunjor <sielerjunjor@gmail.com>
2019-07-22 14:20:54 +0200

    inital commit

---
+++ app.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	_ "encoding/json"
+	"framework-api/models"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"net/http"
+
+	"fmt"
+	. "framework-api/config"
+	. "framework-api/dao"
+	_ "framework-api/models"
+
+
+)
+
+var config = Config{}
+var dao = FrameworksDAO{}
+
+
+//GET list of Frameworks
+func AllFrameworksEndpoint(w http.ResponseWriter, r *http.Request){
+	frameworks, err := dao.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, frameworks)
+}
+// GET a Framework by its ID
+func FindFrameworkById(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	framework, err := dao.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Movie ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, framework)
+}
+
+// POST a new Framework
+func CreateFrameworkEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var framework models.Framework
+	if err := json.NewDecoder(r.Body).Decode(&framework); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	framework.ID = bson.NewObjectId()
+	if err := dao.Insert(framework); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusCreated, framework)
+}
+
+// PUT update an existing Framework
+func UpdateFrameworkEndPoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	defer r.Body.Close()
+	var framework models.Framework
+	if err := json.NewDecoder(r.Body).Decode(&framework); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := dao.Update(params["id"] ,framework); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+// DELETE an existing movie
+func DeleteFrameworkEndPoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	if err := dao.Delete(params["id"]); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+// Parse the configuration file 'config.toml', and establish a connection to DB
+func init() {
+	config.Read()
+
+	dao.Server = config.Server
+	dao.Database = config.Database
+	dao.Connect()
+}
+
+func main() {
+
+	fmt.Println("Starting Rest Endpoint")
+	fmt.Println("Endpoint at localhost:3000/frameworks")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/frameworks", AllFrameworksEndpoint).Methods("GET")
+	r.HandleFunc("/frameworks/{id}", FindFrameworkById).Methods("GET")
+	r.HandleFunc("/frameworks", CreateFrameworkEndPoint).Methods("POST")
+	r.HandleFunc("/frameworks/{id}", UpdateFrameworkEndPoint).Methods("PUT")
+	r.HandleFunc("/frameworks/{id}", DeleteFrameworkEndPoint).Methods("DELETE")
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ dao/framework_dao.go
@@ -0,0 +1,70 @@
+package dao
+
+import (
+	"log"
+
+	"framework-api/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type FrameworksDAO struct {
+	Server   string
+	Database string
+}
+
+var db *mgo.Database
+
+const (
+	COLLECTION = "frameworks"
+)
+
+// Establish a connection to database
+func (m *FrameworksDAO) Connect() {
+	session, err := mgo.Dial(m.Server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = session.DB(m.Database)
+}
+
+
+// Find list of frameworks
+func (m *FrameworksDAO) FindAll() ([]models.Framework, error) {
+	var frameworks []models.Framework
+	err := db.C(COLLECTION).Find(bson.M{}).All(&frameworks)
+	return frameworks, err
+}
+
+// Find a Framework by its id
+func (m *FrameworksDAO) FindById(id string) (models.Framework, error) {
+	var framework models.Framework
+	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&framework)
+	return framework, err
+}
+
+// Insert a movie into database
+func (m *FrameworksDAO) Insert(framework models.Framework) error {
+	err := db.C(COLLECTION).Insert(&framework)
+	return err
+}
+
+// Delete an existing movie
+//func (m *FrameworksDAO) Delete(framework models.Framework) error {
+//	err := db.C(COLLECTION).Remove(&framework)
+//	return err
+//}
+
+//Delete
+func (m *FrameworksDAO) Delete(id string)  error {
+	var framework models.Framework
+	xrr := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&framework)
+	err := db.C(COLLECTION).Remove(xrr)
+	return err
+}
+
+// Update an existing movie
+func (m *FrameworksDAO) Update(id string, framework models.Framework) error {
+	err := db.C(COLLECTION).UpdateId(bson.ObjectIdHex(id), &framework)
+	return err
+}
